de718c775cbbb560ae52ae25674c29f51472a3d1
edupaiva <50725150+edupaiva@users.noreply.github.com>
2021-07-19 11:21:56 -0300

    Update NoticiaController.php

--- app/Http/Controllers/NoticiaController.php
+++ app/Http/Controllers/NoticiaController.php
@@ -8,5 +8,8 @@ use App\Http\Requests;
 
 class NoticiaController extends Controller
 {
-    //
+    public function show(){
+        $noticias = DB::select('select * from noticias');
+        return view('noticia.listagem')->with('noticias', $noticias);
+    }
 }
